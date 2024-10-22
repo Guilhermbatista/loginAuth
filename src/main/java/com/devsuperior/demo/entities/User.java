9b214d85dcc02f6c0d053b2d80bc446ccaa1f3db
Obatista02 <guilherme.santos201412@gmail.com>
2024-10-22 20:14:23 -0300

    add filter

--- src/main/java/com/devsuperior/demo/entities/User.java
+++ src/main/java/com/devsuperior/demo/entities/User.java
@@ -1,9 +1,13 @@
 package com.devsuperior.demo.entities;
 
 import java.time.LocalDate;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -15,9 +19,10 @@ import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 
+@SuppressWarnings("serial")
 @Entity
 @Table(name = "tb_user")
-public class User {
+public class User implements UserDetails {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -76,17 +81,16 @@ public class User {
 	public void addRole(Role role) {
 		roles.add(role);
 	}
+
 	public boolean hasRole(String roleName) {
-		for(Role role : roles) {
-			if(role.getAuthority().equals(roleName)) {
+		for (Role role : roles) {
+			if (role.getAuthority().equals(roleName)) {
 				return true;
 			}
 		}
 		return false;
 	}
 
-	
-	
 	@Override
 	public int hashCode() {
 		return Objects.hash(email, id, name, password, roles);
@@ -103,6 +107,36 @@ public class User {
 		User other = (User) obj;
 		return Objects.equals(email, other.email) && Objects.equals(id, other.id) && Objects.equals(name, other.name)
 				&& Objects.equals(password, other.password) && Objects.equals(roles, other.roles);
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return roles;
+	}
+
+	@Override
+	public String getUsername() {
+		return email;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
 	}
 
 }
